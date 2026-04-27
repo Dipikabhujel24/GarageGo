daf49926000863c20d748068c15b13171971c079
NirjalaGhimire <nirjalaghimire012@gmail.com>
2026-04-28 00:15:01 +0545

    add routes for customer feature pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+
+import CustomerLogin from "./components/Customer/CustomerLogin";
+import CustomerRegister from "./components/Customer/CustomerRegister";
+import CustomerDashboard from "./components/Customer/customerDashboard";
+import CustomerProfile from "./components/Customer/CustomerProfile";
+import CustomerVehicles from "./components/Customer/CustomerVehicles";
+import CustomerHistory from "./components/Customer/CustomerHistory";
+import StaffAddCustomer from "./components/Customer/StaffAddCustomer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<CustomerLogin />} />
+          <Route path="/register" element={<CustomerRegister />} />
+          <Route path="/dashboard" element={<CustomerDashboard />} />
+          <Route path="/profile" element={<CustomerProfile />} />
+          <Route path="/vehicles" element={<CustomerVehicles />} />
+          <Route path="/history" element={<CustomerHistory />} />
+          <Route path="/staff/customers/new" element={<StaffAddCustomer />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
