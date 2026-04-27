3fc4f65b79ddc13f199d6d82c25889ff01d74535
Khushi-cell-web <khushibohora692@gmail.com>
2026-04-27 12:32:44 +0545

    Define Staff model

---
+++ Backend/Models/Staff.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Staff
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(120)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(200)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(255)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(30)]
+    public string Role { get; set; } = "Staff";
+}
