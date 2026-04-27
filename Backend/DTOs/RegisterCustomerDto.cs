1061c8b095da7b4b238c4d1aadefc7a8cc0b6883
NirjalaGhimire <nirjalaghimire012@gmail.com>
2026-04-28 00:13:26 +0545

    feature 12 add customer register dto

---
+++ Backend/DTOs/RegisterCustomerDto.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.DTOs
+{
+    public class RegisterCustomerDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string VehicleMake { get; set; } = string.Empty;
+
+        [Required]
+        public string VehicleModel { get; set; } = string.Empty;
+
+        [Range(1900, 2100)]
+        public int VehicleYear { get; set; }
+
+        public string LicensePlate { get; set; } = string.Empty;
+    }
+}
