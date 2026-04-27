80144f85fb5b85dfda7ef2b56d65f70eaec3f7b2
Khushi-cell-web <khushibohora692@gmail.com>
2026-04-27 13:15:20 +0545

    Fix duplicate CORS configuration in Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,16 +7,7 @@ const string FrontendCorsPolicy = "FrontendCorsPolicy";
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactFrontend", policy =>
-    {
-        policy
-            .WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
+
 
 builder.Services.AddCors(options =>
 {
@@ -36,22 +27,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-<<<<<<< HEAD
-=======
-app.Urls.Clear();
-app.Urls.Add("http://localhost:5000");
->>>>>>> d679d7f63088af4efc9a8092bc7e06eb6c17aa78
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
-<<<<<<< HEAD
 app.UseHttpsRedirection();
-app.UseCors(FrontendCorsPolicy);
-=======
-app.UseCors("ReactFrontend");
->>>>>>> d679d7f63088af4efc9a8092bc7e06eb6c17aa78
+app.UseCors(FrontendCorsPolicy); 
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
